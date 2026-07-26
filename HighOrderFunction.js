a8c804eb19b3edfc4a8f8379576bda3338bc1f89
KISHAN-SINGH <krishansingh6669@gmail.com>
2026-07-26 22:22:50 +0530

    almost done

--- HighOrderFunction.js
+++ HighOrderFunction.js
@@ -1,5 +1,11 @@
 /*
-High-Order-Function 
+High-Order-Function : 
+1. Topic: forEach()
+🤔 What is a Higher-Order Function?
+
+A Higher-Order Function is simply:
+
+A function that takes another function as an argument, or returns another function.
 
 */
 
@@ -38,3 +44,40 @@ for(let i = 0; i < hotel.length; i++){
     console.log(`Available : ${hotel[i].available}`)
     console.log("==========================");
 }
+
+/*
+Now we will print all the deatails with the help of foreach() => high-order-function. 
+
+hotel.forEach(function(item, index){
+    console.log(`${index + 1}. Hotel: ${item.name}`);
+    console.log(`City: ${item.city}`);
+    console.log(`Price: ${item.price}`);
+    console.log(`Rating: ${item.rating}`);
+    console.log(`Available: ${item.available}`);
+    console.log("===============")
+})
+*/
+
+
+/*
+🎫 Jira Ticket #204
+
+Now let's make it feel even more like a real application.
+The product manager says:
+"Only show hotels that are currently available."
+
+Expected output:
+1. Taj Hotel
+Available: true
+
+2. Sea View Resort
+Available: true
+*/
+
+hotel.forEach(function(item, index){
+       if(item.available == true){
+        console.log(`${index + 1}. Hotel : ${item.name}`);
+        console.log(`Available : ${item.available}`)
+        console.log(`=================`)
+       }
+})
