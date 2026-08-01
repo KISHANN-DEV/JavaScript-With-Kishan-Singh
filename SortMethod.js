833cb73216bbf158be4cff826fef3b5b4a7f2f1a
KISHAN-SINGH <krishansingh6669@gmail.com>
2026-08-01 21:07:26 +0530

    Starting Sort() method

---
+++ SortMethod.js
@@ -0,0 +1,8 @@
+/*
+sort() is a built-in array method that rearranges the elements of an array in place (it modifies the original array) and also returns that same sorted array.
+
+Basic syntax:
+
+javascript
+array.sort(compareFunction);
+*/
