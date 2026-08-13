d39cf7edd5b41f6d19f2ab08f4683645febd1f98
KISHAN-SINGH <krishansingh6669@gmail.com>
2026-08-13 19:46:46 +0530

    Starting SetInterval.js

---
+++ SetInterval.js
@@ -0,0 +1,33 @@
+/*
+🚀 Next Topic: setInterval()
+What is setInterval?
+
+setInterval() runs a function repeatedly after a fixed time interval.
+*/
+
+const employees = [
+  {
+    id: 101,
+    name: "Rahul Sharma",
+    department: "Engineering",
+    status: "Online"
+  },
+  {
+    id: 102,
+    name: "Priya Singh",
+    department: "HR",
+    status: "Online"
+  },
+  {
+    id: 103,
+    name: "Amit Kumar",
+    department: "Marketing",
+    status: "Away"
+  },
+  {
+    id: 104,
+    name: "Sneha Verma",
+    department: "Finance",
+    status: "Online"
+  }
+];
