c9ce603c0c85fdc3e2bc53dc133033a8633df594
KISHAN-SINGH <krishansingh6669@gmail.com>
2026-08-16 09:56:39 +0530

    Completed  Async/Await  and using try/catch

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -20,3 +20,112 @@ const employees = [
 ];
 
 
+/*
+.
+
+🎫 Your Task — ASYNC-102
+
+Create:
+
+function getEmployeeSalary(employee)
+
+Requirements:
+
+Delay: 3 seconds
+
+
+Success:
+Salary Loaded: 75000
+
+Then create:
+
+async function loadSalary()
+
+Use:
+
+await getEmployeeSalary(employees[0]);
+
+and print the result.
+*/
+
+
+function getEmployeeSalary(employee){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(employee){
+                resolve(`Salary Loaded : ${employee.salary}`)
+            } else {
+                reject(`Salary not Found....`);
+            }
+        }, 3000);
+    });
+}
+
+async function loadSalary() {
+    const result = await getEmployeeSalary(employees[0]);
+    console.log(result);
+}
+
+loadSalary();
+
+
+/*
+.
+
+🎫 Your Task — TRYCATCH-102
+
+Create:
+
+function getEmployeeDepartment(employee)
+
+Requirements:
+
+Success:
+Department Loaded : Engineering
+
+
+Failure:
+Department Not Found
+
+Then create:
+
+async function loadDepartment()
+
+Use:
+
+try { }
+catch { }
+
+and:
+
+await getEmployeeDepartment(employees[0]);
+*/
+
+function getEmployeeDepartment(employee) {
+    return new Promise((resolve, reject) => {
+        console.log(`Checking Employee Department......`);
+        
+        setTimeout(() => {
+            if(employee){
+                resolve(`Employee Department is : ${employee.department}`)
+            } else {
+                reject(`Employee Department not Found.....`);
+            }
+        }, 2000);
+    });
+}
+
+async function loadDepartment() {
+    try{
+        const checkDept = await getEmployeeDepartment(employees[0]);
+        console.log(checkDept);
+    }
+
+    catch(error) {
+        console.log(error)
+    }
+}
+
+loadDepartment();
+
+
